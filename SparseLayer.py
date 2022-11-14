6c07025ecf66167caba989544efaf8e6d5255968
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2022-11-14 18:17:57 +0100

    added SparseLayer

---
+++ SparseLayer.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+
+class SparseLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
