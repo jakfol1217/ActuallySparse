9a464326590f8e93ca953ae62dbab8b42591b5b5
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2022-11-15 18:40:35 +0100

    added validity check for input

--- SparseModule.py
+++ SparseModule.py
@@ -15,16 +15,24 @@ class SparseLayer(nn.Module):
         self.size_in = size_in
         self.size_out = size_out
         weights = torch.rand(size_in, size_out).to_sparse_coo()
-        #weights = sparse_coo_tensor(size=(size_in, size_out))
+        # weights = sparse_coo_tensor(size=(size_in, size_out))
         self.weights = nn.Parameter(weights)
         self.bias = None
         if bias:
             bias = torch.rand(size_out)
             self.bias = nn.Parameter(bias)
 
-    def forward(self, in_values):
+    def forward(self, in_values: Tensor):
+        if not torch.is_tensor(in_values):
+            raise TypeError("Input must be a Tensor")
+        in_size = self.weights.size()[0]
+        if in_size not in in_values.size():
+            raise Exception("Input values shape does not match")
+        if in_values.size()[0] != in_size:
+            in_values = in_values.t()
+        print(in_values.size())
         out = sparse.mm(self.weights.t(), in_values)
-        if self.bias:
+        if self.bias is not None:
             torch.add(out, self.bias)
         return out
 
@@ -38,9 +46,10 @@ class SparseModuleFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx: Any, *args: Any, **kwargs: Any) -> Any:
         pass
-        #out = sparse.mm(weights, x) + bias
-        #ctx.save_for_backward(out, weights)
-       #return out
+        # out = sparse.mm(weights, x) + bias
+        # ctx.save_for_backward(out, weights)
+
+    # return out
 
     @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:
