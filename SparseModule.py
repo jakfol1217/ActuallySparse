a563fbc396b2ae68060f34693aaf3fc4f336be04
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2022-11-15 17:36:43 +0100

    sparse linear layer with forward

--- SparseModule.py
+++ SparseModule.py
@@ -1,18 +1,32 @@
 from typing import Any
 
 import torch
+import numpy as np
 from torch import Tensor
 from torch import sparse
+from torch import sparse_coo_tensor
 import torch.nn as nn
 
 
 # Klasa implementująca samą warstwę, tzn. m.in. przechowywanie paramterów
 class SparseLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, size_in, size_out, bias=True):
         super(SparseLayer, self).__init__()
+        self.size_in = size_in
+        self.size_out = size_out
+        weights = torch.rand(size_in, size_out).to_sparse_coo()
+        #weights = sparse_coo_tensor(size=(size_in, size_out))
+        self.weights = nn.Parameter(weights)
+        self.bias = None
+        if bias:
+            bias = torch.rand(size_out)
+            self.bias = nn.Parameter(bias)
 
-    def forward(self):
-        pass
+    def forward(self, input):
+        out = sparse.mm(self.weights.t(), input)
+        if self.bias:
+            torch.add(out, self.bias)
+        return out
 
 
 # implementacja funkcjonalności warstwy, a więc przejścia "w przód" oraz "w tył"
@@ -22,10 +36,11 @@ class SparseModuleFunction(torch.autograd.Function):
         pass
 
     @staticmethod
-    def forward(ctx, x: Tensor, weights: Tensor, bias: Tensor) -> Any:
-        out = sparse.mm(weights, x) + bias
-        ctx.save_for_backward(out, weights)
-        return out
+    def forward(ctx: Any, *args: Any, **kwargs: Any) -> Any:
+        pass
+        #out = sparse.mm(weights, x) + bias
+        #ctx.save_for_backward(out, weights)
+       #return out
 
     @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:
