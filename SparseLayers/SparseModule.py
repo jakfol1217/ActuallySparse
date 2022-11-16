e3e762a56ef6ef776e350b581423e4beebd78ce3
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2022-11-16 12:02:06 +0100

    tidying up variable names

--- SparseLayers/SparseModule.py
+++ SparseLayers/SparseModule.py
@@ -10,31 +10,38 @@ import torch.nn as nn
 
 # Klasa implementująca samą warstwę, tzn. m.in. przechowywanie paramterów
 class SparseLayer(nn.Module):
-    def __init__(self, size_in, size_out, bias=True):
+    def __init__(self, in_features, out_features, bias=True, csr_mode=False):
         super(SparseLayer, self).__init__()
-        self.size_in = size_in
-        self.size_out = size_out
-        weights = torch.rand(size_in, size_out).to_sparse_coo()
-        # weights = sparse_coo_tensor(size=(size_in, size_out))
+        self.in_features = in_features
+        self.out_features = out_features
+        #Inicjalizacja wag
+        weights = torch.FloatTensor(in_features, out_features).uniform_(-1, 1)
+        weights[torch.where(abs(weights) <= 0.5)] = 0
+        if csr_mode:
+            weights = weights.to_sparse_csr()
+        else:
+            weights = weights.to_sparse_coo()
         self.weights = nn.Parameter(weights)
+        #Inicjalizacja biasu
         self.bias = None
         if bias:
-            bias = torch.rand(size_out)
+            bias = torch.rand(out_features)
             self.bias = nn.Parameter(bias)
 
     def forward(self, in_values: Tensor):
         if not torch.is_tensor(in_values):
             raise TypeError("Input must be a Tensor")
-        in_size = self.weights.size()[0]
-        if in_size not in in_values.size():
+        if self.in_features not in in_values.size():
             raise Exception("Input values shape does not match")
-        if in_values.size()[0] != in_size:
+        if in_values.size()[0] != self.in_features:
             in_values = in_values.t()
         print(in_values.size())
         out = sparse.mm(self.weights.t(), in_values)
         if self.bias is not None:
             torch.add(out, self.bias)
         return out
+
+
 
 
 # implementacja funkcjonalności warstwy, a więc przejścia "w przód" oraz "w tył"
