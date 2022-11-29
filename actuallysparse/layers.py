fixed gradient problem

actuallysparse/layers.py
@@ -8,20 +8,24 @@ import torch.nn as nn
 
 # Klasa implementująca samą warstwę, tzn. m.in. przechowywanie parametrów
 class SparseLayer(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, csr_mode=False, training=True):
+    def __init__(self, in_features, out_features, bias=True, csr_mode=False):
         super(SparseLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.csr_mode = csr_mode
-        self.training=training
         # Inicjalizacja wag
         weight = torch.FloatTensor(in_features, out_features).uniform_(-1, 1)
         weight[torch.where(abs(weight) <= 0.5)] = 0
         if csr_mode:
             weight = weight.to_sparse_csr()
+            values = weight.values()
+            self.register_buffer('row_indices', weight.crow_indices())
+            self.register_buffer('col_indices', weight.col_indices())
         else:
             weight = weight.to_sparse_coo()
-        self.weight = nn.Parameter(weight)
+            values = weight.values()
+            self.register_buffer('indices', weight.indices())
+        self.values = nn.Parameter(values)
         # Inicjalizacja biasu
         if bias:
             bias = torch.rand(out_features)
@@ -36,9 +40,21 @@ class SparseLayer(nn.Module):
             in_values = in_values.view(-1, 1)
         if self.in_features not in in_values.size():
             raise Exception("Input values shape does not match")
-        if in_values.size()[0] != self.in_features:
+        if in_values.size()[1] != self.in_features:
             in_values = in_values.t()
-        return SparseModuleFunction.apply(in_values, self.weight, self. training, self.bias)
+        if not self.csr_mode:
+            weight = torch.sparse_coo_tensor(values=self.values, indices=self.indices,
+                                             size=(self.in_features, self.out_features)).to_dense()
+        else:
+            weight = torch.sparse_csr_tensor(crow_indices=self.row_indices, col_indices=self.col_indices,
+                                             values=self.values, size=(self.in_features, self.out_features)).to_dense()
+        out = torch.mm(in_values, weight)
+        if self.bias is not None:
+            out = torch.add(out, self.bias)
+        return out
+
+
+        #return SparseModuleFunction.apply(in_values, self.weight, self.bias)
 
     # Funkcja służąca do nadawania nowych wag, głównie przy inicjalizacji
     # ma automatycznie przekształcać na reprezentację rzadką
@@ -57,17 +73,22 @@ class SparseLayer(nn.Module):
             self.bias = nn.Parameter(bias)
 
         if self.csr_mode:
-            self.weight = nn.Parameter(new_weight.to_sparse_csr())
+            weight = new_weight.to_sparse_csr()
+            self.values = nn.Parameter(weight.values())
+            self.register_buffer('row_indices', weight.crow_indices())
+            self.register_buffer('col_indices', weight.col_indices())
             return
-        self.weight = nn.Parameter(new_weight.to_sparse_coo())
+        weight = new_weight.to_sparse_coo()
+        self.values = nn.Parameter(weight.values())
+        self.register_buffer('indices', weight.indices())
 
     # Ustawia k procent najmniejszych wartości na 0
     def prune_smallest_values(self, k=0.1, remove_zeros=True):
         if k < 0 or k > 1:
             raise Exception("K must be a value between 0 and 1")
         with torch.no_grad():
-            values = self.weight.values()
-            values[values < torch.quantile(values, q=k)] = 0
+            values = self.values
+            values[abs(values) < torch.quantile(abs(values), q=k)] = 0
         if remove_zeros:
             self.remove_zeros_from_weight()
 
@@ -76,19 +97,28 @@ class SparseLayer(nn.Module):
     def remove_zeros_from_weight(self):
         if self.csr_mode:
             raise Exception("Cannot remove zeroes with csr mode on")
-        mask = self.weight.values().nonzero().view(-1)
-        new_values = self.weight.values().index_select(0, mask)
-        new_indexes = self.weight.indices().index_select(1, mask)
-        self.weight = nn.Parameter(torch.sparse_coo_tensor(new_indexes, new_values,
-                                                           size=(self.in_features, self.out_features)))
+        mask = self.values.nonzero().view(-1)
+        self.values = nn.Parameter(self.values.index_select(0, mask))
+        self.indices = self.indices.index_select(1, mask)
+        #self.weight = nn.Parameter(torch.sparse_coo_tensor(new_indexes, new_values,
+                                                           #size=(self.in_features, self.out_features)))
+    @property
+    def weight(self):
+        if self.csr_mode:
+            weight = torch.sparse_csr_tensor(crow_indices=self.row_indices, col_indices=self.col_indices,
+                                             values=self.values, size=(self.in_features, self.out_features))
+        else:
+            weight = torch.sparse_coo_tensor(indices=self.indices, values=self.values,
+                                             size=(self.in_features, self.out_features))
+        return weight
 
 # implementacja funkcjonalności warstwy, a więc przejścia "w przód" oraz "w tył"
+"""
 class SparseModuleFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, in_values, weight, training, bias=None):
+    def forward(ctx, in_values, weight, bias=None):
         ctx.save_for_backward(in_values, weight, bias)
-        ctx.training = training
         out = sparse.mm(weight.t(), in_values).t()
         if bias is not None:
             out = torch.add(out, bias)
@@ -97,9 +127,6 @@ class SparseModuleFunction(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         in_values, weight, bias = ctx.saved_tensors
-        training = ctx.training
-        if not training:
-            raise Exception("Training mode is not enabled!")
         grad_in_values = grad_weight = grad_bias = None
         if ctx.needs_input_grad[0]:
             grad_in_values = sparse.mm(weight, grad_output.t())
@@ -109,7 +136,8 @@ class SparseModuleFunction(torch.autograd.Function):
             grad_weight = torch.mm(in_values, grad_output).to_sparse_coo()
         if bias is not None and ctx.needs_input_grad[2]:
             grad_bias = grad_output.sum(0)
-        return grad_in_values, grad_weight, None, grad_bias
+        return grad_in_values, grad_weight, grad_bias
+"""
 
 
 def new_random_basic_coo(in_features, out_features, bias=True):