0c2ef21bc328158c03be5dfd78f99fbca939ae65
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2022-11-29 23:00:20 +0100

    backward prune 1st version

--- actuallysparse/layers.py
+++ actuallysparse/layers.py
@@ -9,11 +9,13 @@ import torch.nn as nn
 
 # Klasa implementująca samą warstwę, tzn. m.in. przechowywanie parametrów
 class SparseLayer(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, csr_mode=False):
+    def __init__(self, in_features, out_features, bias=True, csr_mode=False, k = 0.1, training = True):
         super(SparseLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.csr_mode = csr_mode
+        self.k = k
+        self.training = training
         # Inicjalizacja wag
         weight = torch.FloatTensor(out_features, in_features).uniform_(-1, 1)
         weight[torch.where(abs(weight) <= 0.5)] = 0
@@ -45,6 +47,9 @@ class SparseLayer(nn.Module):
         if in_values.size()[1] != self.in_features:
             in_values = in_values.t()
         if not self.csr_mode:
+            if not self.training:
+                return PruneOnBackward.apply(in_values, self.values, self.indices,
+                                             self.bias, self.in_features, self.out_features, self.k)
             weight = torch.sparse_coo_tensor(values=self.values, indices=self.indices,
                                              size=(self.out_features, self.in_features)).to_dense()
         else:
@@ -82,6 +87,7 @@ class SparseLayer(nn.Module):
         self.register_buffer('indices', weight.indices())
 
     # Ustawia k procent najmniejszych wartości na 0
+    """
     def prune_smallest_values(self, k=0.1, remove_zeros=True):
         if k < 0 or k > 1:
             raise Exception("K must be a value between 0 and 1")
@@ -99,6 +105,8 @@ class SparseLayer(nn.Module):
         mask = self.values.nonzero().view(-1)
         self.values = nn.Parameter(self.values.index_select(0, mask))
         self.indices = self.indices.index_select(1, mask)
+    """
+
 
     @property
     def weight(self):
@@ -111,34 +119,70 @@ class SparseLayer(nn.Module):
         return weight
 
     def __repr__(self):
-        return f"SparseLayer(in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}, csr_mode={self.csr_mode})"
+        return f"SparseLayer(in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}, " \
+               f"csr_mode={self.csr_mode}, k={self.k}, training={self.training})"
+
+    def set_k(self, k):
+        self.k = k
+
+    def set_training(self, training):
+        self.training = training
 
 # implementacja funkcjonalności warstwy, a więc przejścia "w przód" oraz "w tył"
-"""
-class SparseModuleFunction(torch.autograd.Function):
+
+
+class PruneOnBackward(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, in_values, weight, bias=None):
-        ctx.save_for_backward(in_values, weight, bias)
-        out = sparse.mm(weight.t(), in_values).t()
+    def forward(ctx, in_values, values, indices, bias, in_features, out_features, k):
+        ctx.save_for_backward(in_values, values, bias)
+        ctx.indices = indices
+        ctx.k = k
+        ctx.in_features = in_features
+        ctx.out_features = out_features
+        weight = torch.sparse_coo_tensor(values=values, indices=indices,
+                                         size=(out_features, in_features)).to_dense()
+        out = torch.mm(in_values, weight.t())
         if bias is not None:
             out = torch.add(out, bias)
         return out
 
     @staticmethod
     def backward(ctx, grad_output):
-        in_values, weight, bias = ctx.saved_tensors
-        grad_in_values = grad_weight = grad_bias = None
+        in_values, values, bias = ctx.saved_tensors
+        k = ctx.k
+        indices = ctx.indices
+        in_features = ctx.in_features
+        out_features = ctx.out_features
+        grad_in_values = None
         if ctx.needs_input_grad[0]:
-            grad_in_values = sparse.mm(weight, grad_output.t())
-        if ctx.needs_input_grad[1] and weight.is_sparse_csr:
-            grad_weight = torch.mm(in_values, grad_output).to_sparse_csr()
-        elif ctx.needs_input_grad[1] and not weight.is_sparse_csr:
-            grad_weight = torch.mm(in_values, grad_output).to_sparse_coo()
-        if bias is not None and ctx.needs_input_grad[2]:
-            grad_bias = grad_output.sum(0)
-        return grad_in_values, grad_weight, grad_bias
-"""
+            weight = torch.sparse_coo_tensor(indices=indices, values=values,
+                                             size=(out_features, in_features)).to_dense()
+            grad_in_values = torch.mm(grad_output, weight)
+
+        vals, indxs= prune_smallest_values(values, indices, k)
+        values = vals
+        indices = indxs
+        return grad_in_values, None, None, None, None, None, None
+
+
+def prune_smallest_values(values, indices, k=0.1, remove_zeros=True):
+    if k < 0 or k > 1:
+        raise Exception("K must be a value between 0 and 1")
+    with torch.no_grad():
+        values = values
+        values[abs(values) < torch.quantile(abs(values), q=k)] = 0
+    if remove_zeros:
+        values, indices = remove_zeros_from_weight(values, indices)
+    return values, indices
+
+# Usuwa zera z listy wartości wagi, nie działa dla reprezentacji CSR
+# Tworzy nowy tensor rzadki i zapisuje na miejsce starego
+def remove_zeros_from_weight(values, indices):
+    mask = values.nonzero().view(-1)
+    values = nn.Parameter(values.index_select(0, mask))
+    indices = indices.index_select(1, mask)
+    return values, indices
 
 
 def new_random_basic_coo(in_features, out_features, bias=True):
@@ -147,3 +191,7 @@ def new_random_basic_coo(in_features, out_features, bias=True):
 
 def new_random_basic_csr(in_features, out_features, bias=True):
     return SparseLayer(in_features, out_features, bias=bias, csr_mode=True)
+
+
+def new_random_pruning_coo(in_features, out_features, bias=True, k=0.1):
+    return SparseLayer(in_features, out_features, bias=bias, k=k, training=False)
