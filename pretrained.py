81f523adf2acf3bc3de70f230a629239a164fb17
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2022-12-14 11:18:27 +0100

    add dataloaders for cifar100 and caltech256

--- pretrained.py
+++ pretrained.py
@@ -1,7 +1,8 @@
-# File based on https://github.com/huyvnphan/PyTorch_CIFAR10/
+# File based on https://github.com/huyvnphan/PyTorch_CIFAR10/ and https://blog.paperspace.com/vgg-from-scratch-pytorch/
 
 import torch
 import torch.nn as nn
+import torchvision
 
 
 class VGG(nn.Module):
@@ -50,3 +51,38 @@ def vgg11_bn(device="cpu", weights_path = ".weights/state_dicts/vgg11_bn.pt"):
     )
     model.load_state_dict(state_dict)
     return model
+
+def load_cifar10_dataloaders():
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    dataset_train = torchvision.datasets.CIFAR10(".data", download=True, transform=transform)
+    dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=16)
+    dataset_test = torchvision.datasets.CIFAR10(".data", download=True, train=False, transform=transform)
+    dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=16)
+    return dataloader_train, dataloader_test
+
+def load_cifar100_dataloaders():
+    transform = torchvision.transforms.Compose([
+        #transforms.Resize((227,227)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+    dataset_train = torchvision.datasets.CIFAR100(".data", download=True, transform=transform)
+    dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=64)
+    dataset_test = torchvision.datasets.CIFAR100(".data", download=True, train=False, transform=transform)
+    dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=64)
+    return dataloader_train, dataloader_test
+
+def load_caltech256_dataloaders():
+    transform = torchvision.transforms.Compose([
+        #torchvision.transforms.Resize((224, 224)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    dataset = torchvision.datasets.Caltech256(".data", download=True, transform=transform)
+    dataset_train, dataset_test = torch.utils.data.random_split(dataset, [30000, 607])
+    dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=16)
+    dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=16)
+    return dataloader_train, dataloader_test
